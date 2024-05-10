1929f48aa20d0bba7a3d20ddafbf613a60cd1fd4
george42it <george.42.it@gmail.com>
2024-05-10 07:20:50 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+# import flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def base():
+    return render_template("base.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
